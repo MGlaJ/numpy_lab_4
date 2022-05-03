16a639fee6c2120ed8baaa5ea45610a6708dafe0
MGlaJ <165914@student.uwm.edu.pl>
2022-05-03 11:45:26 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,54 @@
+import numpy as np
+
+
+# zad_1
+# a = np.arange(3,46,3)
+# print(a)
+
+# # zad_2
+# lista1 = np.array([1,2,3], dtype='float64')
+# print(lista1)
+# lista2 = lista1.astype('int64')
+# print(lista2)
+
+# zad_3
+# def tablica(n):
+#     lista=np.arange(1,(n*n)+1,1)
+#     return lista.reshape(n,n)
+#
+# print(tablica(4))
+
+# # zad_4
+# def potegi(liczba, ilosc):
+#     return np.logspace(1, ilosc, ilosc, True, liczba)
+#
+# print(potegi(2,4))
+
+# # zad_5
+# def przekatna(n):
+#     w = np.array([a for a in range(n,0,-1)])
+#     return np.diag(w)
+# print(przekatna(3))
+
+# # zad_6
+# def wykreslanka():
+#
+#     poziom = 'WAKACJE'
+#     row = np.array(poziom)
+#     pion = 'WYCIECZKA'
+#     col = np.array(pion)
+#     skos = 'WEEKEND'
+#     diag = np.array(skos)
+#     macierz = np.array((10,10))
+#
+#
+# print(wykreslanka())
+
+
+#zad_7
+n=3
+a=np.arange(2, (2*n)+1,2)
+
+print(a)
+
+
